f5c91ec8ca3f0df9ed04a1ed08695d571439b8e5
Marcus Harbol <marcusharbol@gmail.com>
2024-04-09 16:26:08 -0600

    dart format

--- lib/src/solver/set_solver_base.dart
+++ lib/src/solver/set_solver_base.dart
@@ -7,7 +7,9 @@ SetCard getSetMakingCard(SetCard card1, SetCard card2) {
   final Shape shape = card1.shape.getSetMakingShape(card2.shape);
   final Color color = card1.color.getSetMakingColor(card2.color);
   final Shade shade = card1.shade.getSetMakingShade(card2.shade);
-  final int count = (card1.count == card2.count ? card1.count : 6 - card1.count - card2.count);
+  final int count = (card1.count == card2.count
+      ? card1.count
+      : 6 - card1.count - card2.count);
 
   return SetCard(count, shape, color, shade);
 }
